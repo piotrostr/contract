16b174b0bc73996a64e2845a7db39bc3508be1b1
piotrostr <piotr.ostrowski@aol.com>
2022-07-06 14:21:17 +0200

    add a convenience script to move the contract artifacts

---
+++ regenerate-artifacts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./artifacts/contracts/Vault.sol/Vault.json ../frontend/src/contract
+cp ./typechain/contracts/Vault.ts ../frontend/src/contract
+cp ./typechain/common.ts ../frontend/src/contract
